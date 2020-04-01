a9c2032c2a93f1aa51d531d0d3f43a87cd5d81b6
Robin Appelman <robin@icewind.nl>
2020-04-01 18:46:29 +0200

    basic indexes

--- schema.sql
+++ schema.sql
@@ -6,3 +6,9 @@ CREATE TABLE logs_raw (
 
 ALTER TABLE ONLY logs_raw
     ALTER COLUMN time SET DEFAULT now();
+
+CREATE INDEX logs_raw_id_idx
+    ON logs_raw USING BTREE (id);
+
+CREATE INDEX logs_raw_success_idx
+    ON logs_raw USING BTREE ((json->>'success'));
